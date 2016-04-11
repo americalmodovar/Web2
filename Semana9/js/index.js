c23420896197932f01dd662d1d0e1b0db98cee1e
americalmodovar <america_2701@hotmail.com>
2016-04-11 16:01:52 -0400

    :)

--- Semana9/js/index.js
+++ Semana9/js/index.js
@@ -1,36 +1,29 @@
 var main = function() {
   $('.btn').click(function() {
     var post = $('.status-box').val();
-    $("<li>").text(post).prependTo('.posts');
+    $('<li>').text(post).prependTo('.posts');
     $('.status-box').val('');
-    $('.counter').text('141');
-    $('.btn').addClass('disabled');
+    $('.counter').text('140');
+    $('.btn').addClass('disabled'); 
   });
-  $('.status-box').keypress(function(event) {
-    var post = $('.status-box').val();
-    if (event.which === 13) {
-      event.preventDefault();
-      $("<li>").text(post).prependTo('.posts');
-      $('.status-box').val('').focus();
-      $('.counter').text('140');
-    $('.btn').addClass('disabled');
+  
+  $('.status-box').keyup(function() {
+    var postLength = $(this).val().length;
+    var charactersLeft = 140 - postLength;
+    $('.counter').text(charactersLeft);
+  
+    if(charactersLeft < 0) {
+      $('.btn').addClass('disabled'); 
+    }
+    else if(charactersLeft == 140) {
+      $('.btn').addClass('disabled');
+    }
+    else {
+      $('.btn').removeClass('disabled');
     }
   });
-  $('ul').on('click', 'li', function() {
-    $(this).remove();
-  });
-$('.status-box').keyup(function() {
-  var postLength = $(this).val().length;
-  var charactersLeft = 140 - postLength;
-$('.counter').text(charactersLeft);
- if (charactersLeft <0) {
-     $('.btn').addClass('disabled');} else if (
-         charactersLeft === 140) {
-             $('.btn').addClass('disabled');} else { 
-                $('.btn').removeClass('disabled')};
-});
-$('.btn').addClass('disabled');
-
-};
+  
+  $('.btn').addClass('disabled');
+}
 
 $(document).ready(main);
